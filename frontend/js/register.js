42ae9d23aed69c96a036528123c3ce90439365f8
adrianjafe <145451036+adrianjafe@users.noreply.github.com>
2025-11-20 16:04:14 +0100

    update verification code

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -12,6 +12,8 @@ const form = document.getElementById("registerForm");
 const messageDiv = document.getElementById("register_message");
 const errorcontainer = document.querySelector(".container-error");
 const closebutton = document.getElementById("close-error");
+const codeMessage = document.getElementById("body-code");
+//const number_inputs = 5;
 
 //HandleRegister se encarga de enviar los datos del formulario a la API
 async function handleRegister(event) {
@@ -60,8 +62,8 @@ async function handleRegister(event) {
 
         //Si el registro es exitoso
         if (response.ok) {
-            messageDiv.textContent = `¡Registro exitoso, ${data.usuario.username}!`;
-            messageDiv.style.color = "black";
+            codeMessage.style.display = "flex";
+
             //TODO - Redirigir a página de registro exitoso
         } else {
             //Si el registro falla
@@ -86,6 +88,73 @@ async function handleRegister(event) {
 
 }
 
+//Al cargar la página añade un eventListener para recoger todos los inputs del codigo de verificacion
+document.addEventListener('DOMContentLoaded', () => {
+    // Coge todos los inputs con la clase .code-input
+    const inputs = document.querySelectorAll('.code-input');
+    const errorCodeDiv = document.getElementById("code-error"); 
+
+    //Guarda en la variable inputs todos los inputs y sus valores de la pagina
+    inputs.forEach((input, index) => {
+        //Añade un eventlistener para que escuche al escribir en el input
+        input.addEventListener('input', async () => {
+
+        //Se asegura de que solo se pongan numeros
+        input.value = input.value.replace(/[^0-9]/g, '');
+
+        //Pasa al siguiente input si el actual está lleno
+        if (input.value.length === 1) {
+
+            // Busca el siguiente input en la lista
+            const nextInput = inputs[index + 1];
+            if (nextInput) {
+                nextInput.focus(); // Mueve el cursor al siguiente input
+            }
+        }
+
+        if(index == inputs.length - 1){
+            const code = getFullCode(inputs);
+            if(code.length !== 6){
+                errorCodeDiv.textContent = "Por favor, no has introducido el código bien";
+                return;
+            }
+            try{
+                const respone = await fetch("/v2/register/verify", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json" 
+                    },
+                    body: JSON.stringify({ username, email, password })
+                });
+
+                const result = await response.json();
+                if(response.ok){
+                    window.location.href = "./edit_user.html";
+                } else { 
+                    errorCodeDiv.textContent = result.message || "El código es incorrecto.";
+                }
+            } catch(error){
+                console.log("Error al verificar el código " + error);
+                errorCodeDiv.textContent = 'Error de conexión. Inténtelo de nuevo.'
+            }
+        }
+    });
+
+    // Añade un eventlistener para escuchar si la tecla Backspace ha sido pulsada
+    input.addEventListener('keydown', (e) => {
+        // Si se presiona Backspace y el campo está vacío
+        if (e.key === 'Backspace' && input.value === '') {
+        
+            // Busca el input anterior
+            const prevInput = inputs[index - 1];
+            if (prevInput) {
+                prevInput.focus(); // Mueve el cursor al input anterior
+            }
+        }
+    });
+    });
+});
+
 //Cierra el contenedor de error al hacer click en el botón de cerrar
 closebutton.addEventListener("click", () => {
     errorcontainer.style.display = "none";
@@ -95,6 +164,14 @@ closebutton.addEventListener("click", () => {
 function showError(message){
     errorcontainer.style.display = "flex";
     messageDiv.textContent = message;
+}
+
+function getFullCode(inputs){
+    let code = '';
+    inputs.forEach(input => {
+        code += input.value;
+    })
+    return code;
 }
 
 //Al hacer el submit ejecuta la función de registro
