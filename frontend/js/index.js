2ffd69cce051c46f5be8aff1721694d4a5562880
adrianjafe <145451036+adrianjafe@users.noreply.github.com>
2025-12-26 21:25:04 +0100

    Update things

---
+++ frontend/js/index.js
@@ -0,0 +1,21 @@
+//-----------------------------------
+//   © 2025 OxiGo. Todos los derechos reservados.
+//-----------------------------------
+//   Autor: Adrián Jáuregui
+//   Fecha: 26 de diciembre de 2025
+//-----------------------------------
+//   Fichero: index.js
+//   Descripción: Este fichero maneja el cambio de boton
+//-----------------------------------
+
+const buttons = document.getElementById("session-buttons");
+const username = getUsername();
+
+if(isUserLoggedIn()){
+    buttons.innerHTML =
+        `<button class="profile-button">${username}</button>`;
+} else {
+    buttons.innerHTML =
+        `<a href="./registro.html" class="register-button">Registro</a>
+        <a href="./login.html" class="login-button">Iniciar Sesión</a>`;
+}
